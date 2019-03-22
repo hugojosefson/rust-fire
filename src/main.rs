0ce26d4f4819cdc2fe7278dce143429dda33af44
Hugo Josefson <hugo@josefson.org>
2019-03-22 21:05:47 +0000

    VSync.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use std::time::Duration;
-
-const ONE_SIXTIETH_OF_A_SECOND: Duration = Duration::new(0, 1_000_000_000u32 / 60);
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -17,7 +14,7 @@ pub fn main() {
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
@@ -41,6 +38,5 @@ pub fn main() {
         // The rest of the game loop goes here...
 
         canvas.present();
-        ::std::thread::sleep(ONE_SIXTIETH_OF_A_SECOND);
     }
 }
