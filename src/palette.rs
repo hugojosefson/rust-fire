084292b4812fdfefe90ec5893835a36e06a0fe27
Hugo Josefson <hugo@josefson.org>
2019-03-25 08:00:05 +0000

    Redder palette.

--- src/palette.rs
+++ src/palette.rs
@@ -3,12 +3,11 @@ use crate::constants::*;
 pub fn new() -> [[u8; 4]; PALETTE_SIZE] {
     let mut palette_array: [[u8; 4]; PALETTE_SIZE] = [[0u8; 4]; PALETTE_SIZE];
     for color_index in 0..PALETTE_SIZE {
-        let pixel: [u8; 4] = [
-            (color_index >> 2) as u8,
-            (color_index >> 1) as u8,
-            (color_index) as u8,
-            0u8,
-        ];
+        let f: f32 = color_index as f32;
+        let red = f * 1.2;
+        let green = f / 2.5;
+        let blue = f / 5.;
+        let pixel: [u8; 4] = [blue as u8, green as u8, red as u8, 0u8];
         palette_array[color_index] = pixel;
     }
     return palette_array;
