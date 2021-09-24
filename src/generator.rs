85aaa9c30dbdba11cdc8fe32967d06f271b8688b
Hugo Josefson <hugo@josefson.org>
2021-09-24 23:45:26 +0200

    Implement clippy suggestions.

--- src/generator.rs
+++ src/generator.rs
@@ -6,23 +6,21 @@ fn create_random_value(rng: &mut ThreadRng) -> u32 {
     rng.gen_range(0, 255)
 }
 
-//noinspection RsExternalLinter
 pub fn new(rng: &mut ThreadRng) -> [u32; GENERATOR_SIZE] {
     let mut generator: [u32; GENERATOR_SIZE] = [0u32; GENERATOR_SIZE];
-    for i in 0..GENERATOR_SIZE {
-        generator[i] = create_random_value(rng);
+    for i in generator.iter_mut().take(GENERATOR_SIZE) {
+        *i = create_random_value(rng);
     }
     generator
 }
 
-//noinspection RsExternalLinter
 pub fn cycle(rng: &mut ThreadRng, generator: &mut [u32; GENERATOR_SIZE]) {
-    for i in 0..GENERATOR_SIZE {
-        let value = generator[i];
+    for i in generator.iter_mut().take(GENERATOR_SIZE) {
+        let value = *i;
         if value < 255 {
-            generator[i] = value + 1;
+            *i = value + 1;
         } else {
-            generator[i] = create_random_value(rng);
+            *i = create_random_value(rng);
         }
     }
 }
--- src/palette.rs
+++ src/palette.rs
@@ -10,13 +10,13 @@ fn clamp(f: f32) -> u8 {
 
 pub fn new() -> [[u8; 4]; PALETTE_SIZE] {
     let mut palette_array: [[u8; 4]; PALETTE_SIZE] = [[0u8; 4]; PALETTE_SIZE];
-    for color_index in 0..PALETTE_SIZE {
+    for (color_index, palette_slot) in palette_array.iter_mut().enumerate().take(PALETTE_SIZE) {
         let f: f32 = color_index as f32;
         let red = f * 1.2;
         let green = f / 2.5;
         let blue = f / 5.;
         let pixel: [u8; 4] = [clamp(blue), clamp(green), clamp(red), 0u8];
-        palette_array[color_index] = pixel;
+        *palette_slot = pixel;
     }
     palette_array
 }
