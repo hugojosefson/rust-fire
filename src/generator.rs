411b6a21a94879bc2959258e97d461da0817df40
Hugo Josefson <hugo@josefson.org>
2023-05-10 00:30:04 +0200

    chore: upgrade deps

--- src/generator.rs
+++ src/generator.rs
@@ -3,7 +3,7 @@ use rand::prelude::ThreadRng;
 use rand::Rng;
 
 fn create_random_value(rng: &mut ThreadRng) -> u32 {
-    rng.gen_range(0, 255)
+    rng.gen_range(0..256)
 }
 
 pub fn new(rng: &mut ThreadRng) -> [u32; GENERATOR_SIZE] {
@@ -14,13 +14,14 @@ pub fn new(rng: &mut ThreadRng) -> [u32; GENERATOR_SIZE] {
     generator
 }
 
+// Increase each value in the generator by one.
+// When a value hits the roof at 255, replace it with a new random value.
 pub fn cycle(rng: &mut ThreadRng, generator: &mut [u32; GENERATOR_SIZE]) {
     for i in generator.iter_mut().take(GENERATOR_SIZE) {
-        let value = *i;
-        if value < 255 {
-            *i = value + 1;
-        } else {
+        if *i == 0xff {
             *i = create_random_value(rng);
+        } else {
+            *i += 1;
         }
     }
 }
